4c01552811de5ad89c63c48bda3975f15f837839
jfkquality <jfkquality@gmail.com>
2020-01-30 19:56:25 +0000

    Task 4. Clean up some linter errors.

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Create ABNB API
-"""main app file."""
 from flask import Flask, Blueprint
+"""main app file."""
 
 app = Flask(__name__)
 from models import storage
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # return app_views status as JSON
 from api.v1.views import app_views
+"""Return status of API."""
 
 
 @app_views.route('/status')
