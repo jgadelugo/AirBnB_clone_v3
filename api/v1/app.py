0ed656bc6a4c1f7c799e8101fbd099cdcbe77cc9
jfkquality <jfkquality@gmail.com>
2020-01-30 21:15:51 +0000

    Task 4. Set host, port with getenv(key, default=...).

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """main app file."""
 from flask import Flask, Blueprint
+from os import getenv
 
 app = Flask(__name__)
 from models import storage
 from api.v1.views import app_views
 app.register_blueprint(app_views) # , url_prefix="/api/v1")
 
+host = getenv('HBNB_API_HOST', default='0.0.0.0')
+port = getenv('HBNB_API_PORT', default=5000)
 
-# declare method to handle @app.teardown_appcontext that calls storage.close()
 @app.teardown_appcontext
 def teardown(response_or_exc):
-    """Close storage when done."""
+    """handle @app.teardown_appcontext that calls storage.close()"""
     storage.close()
 
-host = "0.0.0.0"
-port = 5000
 
 if __name__ == "__main__":
     app.run(debug=True, host=host, port=port, threaded=True)
