30fa3e54a9da39ac8681a38cab84b3f21136fe26
jfkquality <jfkquality@gmail.com>
2020-01-31 05:07:12 +0000

    Task 7. Update a state.

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -14,21 +14,38 @@ def states():
             states.append(v.to_dict())
         return (jsonify(states), 200)
     elif request.method == 'POST': 
-        """Create a new State object with request."""
+        #Create a new State object with request.
         if not request.is_json:
             return ("Not a JSON", 400)
         body = request.get_json()
         if 'name' not in body.keys():
             return ("Missing name", 400)
+#        body.save()
         return State(body).to_dict(), 200
 
 @app_views.route('/states/<state_id>', methods=['GET', 'PUT', 'DELETE'])
-def get_state(state_id):
+def state(state_id):
     """Retrieve a state by id."""
+    found = False
+    for v in storage.all('State').values():
+        if v.id == state_id:
+            found = True
+            break
     if request.method == 'GET':
-        for v in storage.all('State').values():
-            if v.id == state_id:
-                return (v.to_dict(), 200)
-        abort(404)
-#    elif request.method == 'PUT':
-        
+        if found:
+            return (v.to_dict(), 200)
+        else:
+            abort(404)
+    # elif request.method == 'PUT':
+    #     #Update a State object
+    #     if not found:
+    #         abort(404)
+    #     if not request.is_json:
+    #         return ("Not a JSON", 400)
+    #     body = request.get_json()
+    #     for key, value in body:
+    #         if key != 'id'  or key != 'created_at' or key != 'updated_at':
+    #             setattr(v, key, value)
+    #     v.save()
+    #     return (v, 200)
+
