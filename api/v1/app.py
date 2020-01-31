25e88fe9ea3520dddd2bd01208bd89650d8eb62c
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-01-31 00:58:58 +0000

    Task 6. Pep8 errors.

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,15 +6,17 @@ from os import getenv
 app = Flask(__name__)
 from models import storage
 from api.v1.views import app_views
-app.register_blueprint(app_views) # , url_prefix="/api/v1")
+app.register_blueprint(app_views)  # , url_prefix="/api/v1")
 
 host = getenv('HBNB_API_HOST', default='0.0.0.0')
 port = getenv('HBNB_API_PORT', default=5000)
+
 
 @app.teardown_appcontext
 def teardown(response_or_exc):
     """handle @app.teardown_appcontext that calls storage.close()"""
     storage.close()
+
 
 @app.errorhandler(404)
 def not_found():
