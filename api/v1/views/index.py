36630e4ecb4e4e03c753d75739b4853898cc5a60
jfkquality <jfkquality@gmail.com>
2020-01-31 00:19:59 +0000

    Task 5. Init commit.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 """Return status of API."""
 from api.v1.views import app_views
-
-
+from models.storage import count
 @app_views.route('/status')
 def appviews():
     """Get status of API."""
-    return {
-        "status": "OK",
-    }
+    return {"status": "OK"}
+
+
+@app_view.route('/stats')
+def stats():
+    """retrieves the number of each objects by type."""
+    counts = {
+        "amenities": count("Amenities"), 
+        "cities": count("Cities"),
+        "places": count("Places"),
+        "reviews": count("Reviews"),
+        "states": count("States"),
+        "users": count("Users")
+}
+   return counts
